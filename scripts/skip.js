26880e46bac3818a5bce46d75a1c483e296834aa
Filipe Batista <filipe.x@live.com>
2021-01-07 10:16:42 -0300

    template

---
+++ scripts/skip.js
@@ -0,0 +1,9 @@
+
+process.exit(0);
+
+// Colocar a variavel de ambiente SKIP_BUILD com o valor 1. caso não funcione basta deixar como está
+// if (process.env.SKIP_BUILD) {
+//     process.exit(0);
+// } else {
+//     process.exit(1);
+// }
